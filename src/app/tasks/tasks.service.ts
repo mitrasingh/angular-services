5fb29193264dab8ce6422b05a5723541009a0ca1
mitrasingh <mitra.coder@gmail.com>
2024-08-19 21:10:42 -0400

    add: new method which adds a task to task array

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -6,4 +6,13 @@ import { Task } from './task.model';
 })
 export class TaskService {
   tasks = signal<Task[]>([]);
+
+  addTask(taskData: { title: string; description: string }) {
+    const newTask: Task = {
+      ...taskData,
+      id: Math.random().toString(),
+      status: 'OPEN',
+    };
+    this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+  }
 }
