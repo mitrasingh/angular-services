fd4124803f171d23f05a5b7a5de0cb3226c98420
mitrasingh <mitra.coder@gmail.com>
2024-08-20 17:36:38 -0400

    add: constructor for dependancy injection of TaskService into component

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TaskService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -10,6 +11,8 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
+
+  constructor(private taskService: TaskService) {}
 
   onAddTask(title: string, description: string) {
     this.formEl()?.nativeElement.reset();
